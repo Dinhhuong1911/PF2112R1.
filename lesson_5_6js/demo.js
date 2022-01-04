3759bc21b9410157e744a874691ad037554c4f86
đình hương <dinhhuong1911@gmail.com>
2022-01-04 21:50:19 +0700

    bài tập

---
+++ lesson_5_6js/demo.js
@@ -0,0 +1,1 @@
+// document.write("xin chào các bạn")
---
+++ lesson_5_6js/thuchanh.js
@@ -0,0 +1,20 @@
+// function showMessage(){
+//     alert ('Xin chào!');
+// }
+
+let inputWidth = prompt ("Enter the width");
+let inputHeight = prompt("Enter the height");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("The area is:" + area);
+
+// phần coment dươi đây cũng hiển thi tương tự
+// let inputWidth;
+// let inputHeight;
+// inputWidth = prompt("Enter the width");
+// inputHeight = prompt("Enter the height");
+// let width = parseInt(inputWidth);
+// let height = parseInt(inputHeight);
+// let area = width * height;
+// document.write("The area is: " + area);
